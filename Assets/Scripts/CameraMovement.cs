323e7eabfa0909d44e2b7bc90d2c784d7d026cdb
NorkUA <ICBazalev@gmail.com>
2023-06-26 13:03:14 +0300

    LT
    lt

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -11,6 +11,8 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]
     private GameObject character;
     [SerializeField]
+
+    //Last Try
     //Checking 4444555
     //SAFASFSFAASFSAFSFASFFSAFSA
     public Vector3 GetOffsetPosition() { return offsetPosition; }
