d0d0bf205941132d9819e3fc9689f2084ad2bfc2
Sk0rd1 <tyschenko.mykhailo@gmail.com>
2023-07-01 21:50:19 +0300

    01.07.2023:21:50
    Підйом на бокс ще не зробив, бо вечеряв. Хейтери SOSATb

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool isOpen;
+
+    void Start()
+    {
+        
+    }
+
+    public void Open()
+    {
+        isOpen = true;
+    }
+
+    void Update()
+    {
+        if(isOpen)
+        {
+            Transform cube = transform.GetChild(0);
+            if (cube.transform.position.y > -7)
+                cube.transform.position += new Vector3(0f, -5f * Time.deltaTime, 0f);
+        }
+    }
+}
